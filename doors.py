2dcc0dcf0548ad374b4e9816181ceeb838a242e5
oisteing <66906363+oisteing@users.noreply.github.com>
2025-03-14 11:53:46 +0100

    Update doors.py

--- doors.py
+++ doors.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 import streamlit.components.v1 as components
 
-components.iframe("https://scratch.mit.edu/projects/485450858/fullscreen/", height=500)
+components.iframe("https://scratch.mit.edu/projects/485450858/fullscreen/")
